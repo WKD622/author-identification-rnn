fab58bc668c158be3311d1e6ee531fef9ab38985
WKD622 <kuba.ziarko@gmail.com>
2019-08-05 11:04:23 +0200

    preprocessing

---
+++ library/preprocessing/conversion_to_tensor/convert.py
@@ -0,0 +1,33 @@
+from typing import List
+
+import torch
+
+
+def char_to_tensor(char, alphabet):
+    tensor = torch.zeros(len(alphabet))
+    for counter, element in enumerate(alphabet):
+        if element == char:
+            tensor[counter] = 1
+    return tensor
+
+
+def text_to_tensor(text: str, alphabet: List):
+    tensor = torch.LongTensor(len(text), len(alphabet))
+    for counter, char in enumerate(text):
+        tensor[counter] = char_to_tensor(char, alphabet)
+    return tensor
+#
+# from library.preprocessing.conversion_to_tensor.alphabets.en import alphabet as en_alphabet
+# from library.files.files import save_to_file, FileLoader
+# from library.preprocessing.characters_mapping.map import map_characters
+# from library.preprocessing.characters_mapping.mappers.en import charmap as en
+# path = "../../../data/authors/EN001/known01.txt"
+# text = ""
+# f = open(path, "r")
+# save_to_file("", "out.txt", map_characters(en, f))
+# f.close()
+#
+# fl = FileLoader("out.txt")
+# file, _ = fl.load_file()
+# torch.set_printoptions(profile="full")
+# print(text_to_tensor(file, en_alphabet))
---
+++ library/preprocessing/preprocessing.py
@@ -0,0 +1,11 @@
+class Preprocessing():
+    authors = []
+
+    def __init__(self, path):
+        self.path = path
+
+    def save_to_files(self):
+        pass
+
+    def do_preprocessing(self):
+        pass
