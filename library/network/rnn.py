1eaba386174aca79024dfcc39d4e592142a187f9
WKD622 <kuba.ziarko@gmail.com>
2019-10-18 12:59:04 +0200

    last correction to rnn

--- library/network/rnn.py
+++ library/network/rnn.py
@@ -5,14 +5,15 @@ from torch.nn.utils import clip_grad_norm
 from library.network.batch_processing.batching import BatchProcessor
 from library.preprocessing.to_tensor.alphabets.en_alphabet import alphabet as en
 
-hidden_size = 1024
-num_layers = 1
-num_epochs = 20
-batch_size = 6
-timesteps = 30
-learning_rate = 0.002
+hidden_size = 100
+num_layers = 2
+num_epochs = 5
+batch_size = 40
+timesteps = 50
+learning_rate = 0.004
 authors_size = 100
 vocab_size = len(en)
+save_path = "result"
 
 
 class TextGenerator(nn.Module):
@@ -34,27 +35,31 @@ loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 batch_processor = BatchProcessor(batch_size=batch_size, authors_size=authors_size, timesteps=timesteps)
 
-for epoch in range(num_epochs):
-    states = (torch.zeros(num_layers, batch_size, hidden_size),
-              torch.zeros(num_layers, batch_size, hidden_size))
-
+while True:
+    j = 0
     i = 0
-    batch_processor.new_epoch()
-    while batch_processor.next_batch():
+    j += 1
+    print(j)
+    for epoch in range(num_epochs):
+        states = (torch.zeros(num_layers, batch_size, hidden_size),
+                  torch.zeros(num_layers, batch_size, hidden_size))
+
         i += 1
-        batches = batch_processor.get_results()[0]
-        labels = batch_processor.get_results()[1]
-        batches = batches.type(torch.FloatTensor)
-        target = torch.tensor(labels)
-        outputs, _ = model(batches, states)
-        loss = loss_fn(outputs, target)
+        batch_processor.new_epoch()
+        while batch_processor.next_batch():
+            batches = batch_processor.get_results()[0]
+            labels = batch_processor.get_results()[1]
+            batches = batches.type(torch.FloatTensor)
+            target = torch.tensor(labels)
+            outputs, _ = model(batches, states)
+            loss = loss_fn(outputs, target)
 
-        model.zero_grad()
-        loss.backward()
-        clip_grad_norm(model.parameters(), 0.5)
-        optimizer.step()
+            model.zero_grad()
+            loss.backward()
+            clip_grad_norm(model.parameters(), 0.5)
+            optimizer.step()
 
-        step = (i + 1) // timesteps
+            step = (i + 1) // timesteps
 
-        if step % 1 == 0:
-            print('Epoch [{}/{}], Loss: {:4f}'.format(epoch + 1, num_epochs, loss.item()))
+        print('Epoch [{}/{}], Loss: {:4f}'.format(epoch + 1, num_epochs, loss.item()))
+    torch.save(model.state_dict(), save_path)
