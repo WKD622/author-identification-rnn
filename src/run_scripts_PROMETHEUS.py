5dd5a97aa8d7b4964607312466e525093cc42281
WKD622 <kuba.ziarko@gmail.com>
2019-10-28 14:07:50 +0100

    repait running script:

--- src/run_scripts_PROMETHEUS.py
+++ src/run_scripts_PROMETHEUS.py
@@ -4,4 +4,4 @@ from os import walk, system
 f = []
 for (_, _, filenames) in walk(sys.argv[1]):
     for filename in filenames:
-        system('sbatch ./scripts/' + filename)
+        system('sbatch ' + filename)
