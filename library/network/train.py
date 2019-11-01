2d10e30476ebc6104cdf5b1218521c4c793891a3
WKD622 <kuba.ziarko@gmail.com>
2019-11-01 18:45:25 +0100

    asdnaskd

--- library/network/train.py
+++ library/network/train.py
@@ -125,7 +125,6 @@ class OutputManager:
     HEADLINE = MODEL + SEPARATOR + EPOCH + SEPARATOR + LOSS + SEPARATOR + ACCURACY + SEPARATOR + TIME_PASSED + '\n'
     MODELS_FOLDER_NAME = 'models'
 
-
     def __init__(self, save_path, hidden_size, num_layers, num_epochs, batch_size, timesteps, learning_rate,
                  authors_size, vocab_size):
         self.models_path = os.path.join(save_path, self.MODELS_FOLDER_NAME)
@@ -139,6 +138,8 @@ class OutputManager:
         self.learning_rate = learning_rate
         self.authors_size = authors_size
         self.vocab_size = vocab_size
+        self.min_loss = 1000
+        self.max_accuracy = 0.0
 
         self.initialize_files()
         self.outputs_counter = 1
@@ -146,9 +147,11 @@ class OutputManager:
     def next_output(self, model, losses, accuracy, epoch_number, time_passed):
         formatted_time_passed = str(datetime.timedelta(seconds=time_passed))
         loss_avg = sum(losses) / len(losses)
-        self.save_model(model)
+        if (loss_avg <= self.min_loss or accuracy >= self.max_accuracy) and self.outputs_counter > 25:
+            self.save_model(model)
         self.console_output(loss_avg, accuracy, epoch_number, time_passed=formatted_time_passed)
         self.file_output(loss_avg, accuracy, epoch_number, time_passed=formatted_time_passed)
+        self.update_max_loss_and_accuracy(loss=loss_avg, accuracy=accuracy)
         self.outputs_counter += 1
 
     def console_output(self, loss_avg, accuracy, epoch_number, time_passed):
@@ -193,3 +196,9 @@ class OutputManager:
                        'num_epochs: ' + str(self.num_epochs) + '\n' +
                        'vocab_size: ' + str(self.vocab_size) + '\n' +
                        'authors_size: ' + str(self.authors_size) + '\n')
+
+    def update_max_loss_and_accuracy(self, loss, accuracy):
+        if loss < self.min_loss:
+            self.min_loss = loss
+        if accuracy > self.max_accuracy:
+            self.max_accuracy = accuracy
