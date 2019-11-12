143851fca9932dec3e0dd1b2c90dd687dcca6cee
WKD622 <kuba.ziarko@gmail.com>
2019-11-12 19:36:11 +0100

    lkasmdlkas

--- library/network/model.py
+++ library/network/model.py
@@ -5,7 +5,7 @@ class TextGenerator(nn.Module):
 
     def __init__(self, authors_size, vocab_size, hidden_size, num_layers, timesteps):
         super(TextGenerator, self).__init__()
-        self.lstm = nn.LSTM(vocab_size, hidden_size, num_layers, batch_first=True)
+        self.lstm = nn.LSTM(vocab_size, hidden_size, num_layers, batch_first=True, dropout=0.2)
         self.linear = nn.Linear(timesteps * hidden_size, authors_size)
 
     def forward(self, x, h):
